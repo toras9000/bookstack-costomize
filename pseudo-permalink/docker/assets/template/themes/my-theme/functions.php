1009aa4079c8edbf8a60d11babcbdbec737d6d99
toras9000 <toras9000@example.com>
2024-01-01 00:27:53 +0900

    Redirect by routing.

--- pseudo-permalink/docker/assets/template/themes/my-theme/functions.php
+++ pseudo-permalink/docker/assets/template/themes/my-theme/functions.php
@@ -2,29 +2,34 @@
 
 use BookStack\Facades\Theme;
 use BookStack\Theming\ThemeEvents;
-use BookStack\Entities\Models\Bookshelf;
-use BookStack\Entities\Models\Book;
-use BookStack\Entities\Models\Chapter;
-use BookStack\Entities\Models\Page;
-use Illuminate\Http\Request;
+use BookStack\Entities\Controllers;
+use BookStack\Entities\Models;
+use Illuminate\Routing\Router;
 
-Theme::listen(ThemeEvents::WEB_MIDDLEWARE_BEFORE , function(Request $request)
+// Register a permalink endpoint
+Theme::listen(ThemeEvents::ROUTES_REGISTER_WEB, function (Router $router)
 {
-    $req_path = $request->path();
-    if (preg_match('!^/?link/(shelves|books|chapters|pages)/(\d+)$!', $req_path,  $matches))
+    $router->get('/link/shelves/{id}', function (string $id)
     {
-        $req_category = $matches[1];
-        $req_id = $matches[2];
-        switch ($req_category)
-        {
-        case 'shelves':  $entity = Bookshelf::query()->where('id', '=', $req_id)->first(); break;
-        case 'books':    $entity = Book::query()->where('id', '=', $req_id)->first();      break;
-        case 'chapters': $entity = Chapter::query()->where('id', '=', $req_id)->first();   break;
-        case 'pages':    $entity = Page::query()->where('id', '=', $req_id)->first();      break;
-        }
-        if (isset($entity))
-        {
-            return redirect($entity->getUrl());
-        }
-    }
+        $entity = Models\Bookshelf::query()->where('id', '=', $id)->first();
+        return isset($entity) ? redirect($entity->getUrl()) : abort(404);
+    })->whereNumber('id');
+
+    $router->get('/link/books/{id}', function (string $id)
+    {
+        $entity = Models\Book::query()->where('id', '=', $id)->first();
+        return isset($entity) ? redirect($entity->getUrl()) : abort(404);
+    })->whereNumber('id');
+
+    $router->get('/link/chapters/{id}', function (string $id)
+    {
+        $entity = Models\Chapter::query()->where('id', '=', $id)->first();
+        return isset($entity) ? redirect($entity->getUrl()) : abort(404);
+    })->whereNumber('id');
+
+    $router->get('/link/pages/{id}', function (string $id)
+    {
+        $entity = Models\Page::query()->where('id', '=', $id)->first();
+        return isset($entity) ? redirect($entity->getUrl()) : abort(404);
+    })->whereNumber('id');
 });
